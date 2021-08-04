88d53bfb85c2184d56ad8855ef7f4a89a1bd37a1
ysricky <yosuaricky2606@gmail.com>
2021-08-04 14:36:35 +0700

    add template files

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// const chooseOpponentBtn = document.querySelector('#choose-opponent-button');
+// const player2TextInput = document.querySelector('#player2-name');
+
+// chooseOpponentBtn.addEventListener('click', () => {
+//   if (chooseOpponentBtn.textContent === 'VS PLAYER') {
+//     chooseOpponentBtn.textContent = 'VS AI';
+//     player2TextInput.removeAttribute('disabled');
+//   } else {
+//     chooseOpponentBtn.textContent = 'VS PLAYER';
+//     player2TextInput.setAttribute('disabled', 'disabled');
+//   }
+// });
