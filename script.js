7fc738f4bbfb88d879a07e808252fbff017b1029
ysricky <yosuaricky2606@gmail.com>
2021-08-20 18:13:25 +0700

    create game logic

--- script.js
+++ script.js
@@ -1,124 +1,246 @@
 'use strict';
 
-(function () {
+const game = (() => {
   //FUNCTION FACTORY to create Player Object
-  const Player = (name, pawn) => {
-    return { name, pawn };
+  const Player = (name, marker) => {
+    const getMarker = () => marker;
+    const shoutTurn = () => `${name} turn...`;
+    const shoutWin = () => `${name} won this match!`;
+    return { getMarker, shoutTurn, shoutWin };
   };
 
   //MODULE for providing anything related to board
   const GameBoard = (() => {
     let board = ['', '', '', '', '', '', '', '', ''];
-    const winCondition = () => {
-      if (
-        (board[0] === 'X' && board[1] === 'X' && board[2] === 'X') ||
-        (board[3] === 'X' && board[4] === 'X' && board[5] === 'X') ||
-        (board[6] === 'X' && board[7] === 'X' && board[8] === 'X') ||
-        (board[0] === 'X' && board[3] === 'X' && board[6] === 'X') ||
-        (board[1] === 'X' && board[4] === 'X' && board[7] === 'X') ||
-        (board[2] === 'X' && board[5] === 'X' && board[8] === 'X') ||
-        (board[0] === 'X' && board[4] === 'X' && board[8] === 'X') ||
-        (board[2] === 'X' && board[4] === 'X' && board[6] === 'X')
-      ) {
-        DisplayController.message.textContent = 'X WIN (restart game?)';
-      } else if (
-        (board[0] === 'O' && board[1] === 'O' && board[2] === 'O') ||
-        (board[3] === 'O' && board[4] === 'O' && board[5] === 'O') ||
-        (board[6] === 'O' && board[7] === 'O' && board[8] === 'O') ||
-        (board[0] === 'O' && board[3] === 'O' && board[6] === 'O') ||
-        (board[1] === 'O' && board[4] === 'O' && board[7] === 'O') ||
-        (board[2] === 'O' && board[5] === 'O' && board[8] === 'O') ||
-        (board[0] === 'O' && board[4] === 'O' && board[8] === 'O') ||
-        (board[2] === 'O' && board[4] === 'O' && board[6] === 'O')
-      ) {
-        DisplayController.message.textContent = 'O WIN (restart game?';
-      }
-    };
-    return { board, winCondition };
+    const winPattern = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    return { board, winPattern };
   })();
 
-  //MODULE for providing DOM elements to other modules / functions
+  //MODULE for providing games logic
+  const gameFlowController = (() => {
+    let gameOn;
+    let player1;
+    let player2;
+    let playerAI;
+    let turn = 0;
+
+    const createPlayers = (playerName, playerMarker) => {
+      const player = Player(playerName, playerMarker);
+      return player;
+    };
+
+    const checkWinner = () => {
+      let winner;
+      for (let i = 0; i < GameBoard.board.length; i++) {
+        if (GameBoard.board[i] === '') continue;
+        for (let j = 0; j < GameBoard.winPattern.length; j++) {
+          if (
+            GameBoard.board[GameBoard.winPattern[j][0]] ===
+              GameBoard.board[i] &&
+            GameBoard.board[GameBoard.winPattern[j][1]] ===
+              GameBoard.board[i] &&
+            GameBoard.board[GameBoard.winPattern[j][2]] === GameBoard.board[i]
+          ) {
+            if (GameBoard.board[i] === 'X') {
+              winner = player1;
+            } else {
+              player2 ? (winner = player2) : (winner = playerAI);
+            }
+            DisplayController.displayMessage(winner.shoutWin().toUpperCase());
+            gameOn = false;
+            break;
+          }
+        }
+        // if (gameOn === false && winner === undefined) {
+        //   DisplayController.displayMessage('TIE GAME!!!');
+        //   console.log(winner);
+        //   gameOn = false;
+        //   break;
+        // } else if (winner) {
+        //   break;
+        // }
+      }
+    };
+
+    const startGame = () => {
+      if (DisplayController.checkInputs()) {
+        if (DisplayController.playerTwoName.value === 'AI') {
+          playerAI = createPlayers(DisplayController.playerTwoName.value, 'O');
+        } else {
+          player2 = createPlayers(DisplayController.playerTwoName.value, 'O');
+        }
+        player1 = createPlayers(DisplayController.playerOneName.value, 'X');
+        DisplayController.disableInputs();
+        DisplayController.displayMessage(player1.shoutTurn());
+        gameOn = true;
+      } else {
+        DisplayController.displayMessage('Please enter player name');
+      }
+    };
+
+    const checkAvailableField = () => {
+      const availableField = GameBoard.board.reduce(function (
+        acc,
+        curr,
+        index
+      ) {
+        if (curr === '') {
+          acc.push(index);
+        }
+        return acc;
+      },
+      []);
+      const randomField = () =>
+        Math.floor(Math.random() * availableField.length);
+      return availableField[randomField()];
+    };
+
+    const player1Turn = (field) => {
+      field.target.textContent = player1.getMarker();
+      GameBoard.board[Number(field.target.getAttribute('id'))] =
+        player1.getMarker();
+      player2
+        ? DisplayController.displayMessage(player2.shoutTurn())
+        : DisplayController.displayMessage(playerAI.shoutTurn());
+      checkWinner();
+      turn++;
+    };
+
+    const player2Turn = (field) => {
+      field.target.textContent = player2.getMarker();
+      GameBoard.board[Number(field.target.getAttribute('id'))] =
+        player2.getMarker();
+      DisplayController.displayMessage(player1.shoutTurn());
+      checkWinner();
+      turn++;
+    };
+
+    const playerAITurn = () => {
+      if (playerAI && gameOn) {
+        console.log(gameOn);
+        const aiChoice = checkAvailableField();
+        DisplayController.fields[aiChoice].textContent = playerAI.getMarker();
+        GameBoard.board[aiChoice] = playerAI.getMarker();
+        DisplayController.displayMessage(player1.shoutTurn());
+        checkWinner();
+        turn--;
+      } else {
+        return;
+      }
+    };
+
+    const placeMarkerOnTurn = (field) => {
+      if (turn % 2 === 0) {
+        player1Turn(field);
+        playerAITurn();
+      } else {
+        player2Turn(field);
+      }
+    };
+
+    const setMarker = (field) => {
+      if (gameOn && field.target.textContent === '') placeMarkerOnTurn(field);
+    };
+
+    const resetGameFlow = () => {
+      turn = 0;
+      gameOn = false;
+      player1 = undefined;
+      player2 = undefined;
+      playerAI = undefined;
+      GameBoard.board = ['', '', '', '', '', '', '', '', ''];
+    };
+
+    return { startGame, setMarker, resetGameFlow };
+  })();
+
+  //MODULE for providing DOM elements interactions
   const DisplayController = (() => {
     const playerOneName = document.querySelector('#player1-name');
     const playerTwoName = document.querySelector('#player2-name');
+    const btnChooseOpponent = document.querySelector('#choose-opponent-button');
     const btnStart = document.querySelector('#start-button');
     const btnRestart = document.querySelector('#restart-button');
     const message = document.querySelector('h2');
     const fields = document.querySelectorAll('.field');
 
-    return {
-      playerOneName,
-      playerTwoName,
-      btnStart,
-      btnRestart,
-      message,
-      fields,
-    };
-  })();
-
-  //MODULE for providing games logic
-  const GameFlowController = (() => {
-    let turn = 0;
-    let player;
-    //creating player object
-
-    const playerOne = Player(`${DisplayController.playerOneName.value}`, 'X');
-    const playerTwo = Player(`${DisplayController.playerTwoName.value}`, 'O');
-
-    const checkName = () =>
-      DisplayController.playerOneName.value &&
-      DisplayController.playerTwoName.value
+    const checkInputs = () => {
+      return playerOneName.value !== '' && playerTwoName.value !== ''
         ? true
         : false;
+    };
 
-    const displayPlayerTurn = () => {
-      switch (turn % 2) {
-        case 0:
-          DisplayController.message.textContent = `${playerOne.name}'s turn`;
-          break;
-        case 1:
-          DisplayController.message.textContent = `${playerTwo.name}'s turn`;
+    const chooseOpponent = () => {
+      if (btnChooseOpponent.textContent === 'VS AI') {
+        playerTwoName.setAttribute('disabled', 'disabled');
+        playerTwoName.value = 'AI';
+        btnChooseOpponent.textContent = 'VS PLAYER';
+        displayMessage('Player VS AI Mode');
+      } else {
+        playerTwoName.removeAttribute('disabled', 'disabled');
+        playerTwoName.value = '';
+        playerTwoName.setAttribute('placeholder', 'Player 2');
+        btnChooseOpponent.textContent = 'VS AI';
+        displayMessage('Player VS Player Mode');
       }
     };
 
-    const placePawn = (field, player) => {
-      switch (turn % 2) {
-        case 0:
-          player = playerOne;
-          turn++;
-          break;
-        case 1:
-          player = playerTwo;
-          turn++;
-          break;
-      }
-      field.target.textContent = player.pawn;
-      console.log(field);
-      GameBoard.board[parseInt(field.target.getAttribute('data-field'))] =
-        player.pawn;
-      displayPlayerTurn();
-      GameBoard.winCondition();
-      console.log(GameBoard.board);
+    const displayMessage = (text) => (message.textContent = text);
+
+    const disableInputs = () => {
+      btnStart.setAttribute('disabled', 'disabled');
+      btnChooseOpponent.setAttribute('disabled', 'disabled');
+      playerOneName.setAttribute('disabled', 'disabled');
+      playerTwoName.setAttribute('disabled', 'disabled');
     };
 
-    const provideField = () => {
-      DisplayController.fields.forEach((field) => {
-        field.addEventListener('click', placePawn, { once: true });
+    const resetInputs = () => {
+      if (btnChooseOpponent.textContent === 'VS PLAYER')
+        playerTwoName.value = 'AI';
+      playerOneName.value = '';
+      playerTwoName.value = '';
+    };
+
+    const restartGame = () => {
+      gameFlowController.resetGameFlow();
+      resetInputs();
+      displayMessage('WELCOME TO TIC-TAC-TOE');
+      playerOneName.removeAttribute('disabled');
+      playerTwoName.removeAttribute('disabled');
+      btnStart.removeAttribute('disabled');
+      btnChooseOpponent.removeAttribute('disabled');
+      btnChooseOpponent.textContent = 'VS AI';
+      fields.forEach((field) => {
+        field.textContent = '';
       });
     };
 
-    const startGame = () => {
-      DisplayController.btnStart.addEventListener(
-        'click',
-        () => {
-          displayPlayerTurn();
-          provideField();
-        },
-        {
-          once: true,
-        }
-      );
+    // event listener
+    btnStart.addEventListener('click', gameFlowController.startGame);
+    btnRestart.addEventListener('click', restartGame);
+    btnChooseOpponent.addEventListener('click', chooseOpponent);
+    fields.forEach((field) => {
+      field.addEventListener('click', gameFlowController.setMarker);
+    });
+
+    resetInputs();
+
+    return {
+      playerOneName,
+      playerTwoName,
+      fields,
+      checkInputs,
+      disableInputs,
+      displayMessage,
     };
-    startGame();
   })();
 })();
